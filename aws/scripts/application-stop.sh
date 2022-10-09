fa1722e493eb2f7fadc4cc17bb79f9e28fdfd0d0
Joe Galley <joegalley@users.noreply.github.com>
2022-10-09 13:44:59 -0400

    update

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Kill the running JAR file
-app_id=$(ps -ef | grep 'spring-petclinic-metrics.jar' | grep -v 'grep' | awk '{ printf $2 }')
-sudo kill $app_id
+# app_id=$(ps -ef | grep 'spring-petclinic-metrics.jar' | grep -v 'grep' | awk '{ printf $2 }')
+# sudo kill $app_id
