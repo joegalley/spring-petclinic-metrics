15e571e9c25ec419fe4694d86ff9fb0cd5ceb0c4
Joe Galley <joegalley@users.noreply.github.com>
2022-10-09 13:21:32 -0400

    update

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,4 +2,4 @@
 set -xe
 
 # Copy JAR file from S3 bucket to EC2 instance
-aws s3 cp s3://terraform-20221009151650560700000002/spring-petclinic-2.6.0.jar /usr/local/spring-petclinic-metrics.jar
+aws s3 cp s3://terraform-20221009165540466200000002/spring-petclinic-2.6.0.jar /usr/local/spring-petclinic-metrics.jar
