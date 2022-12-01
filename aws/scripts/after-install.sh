1a2f820d7fbb497c088dfe716c0878eb43c1b776
joe <joegalley@users.noreply.github.com>
2022-11-30 22:29:00 -0500

    update S3 bucket

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-txq68ilhai81/spring-petclinic-2.7.3.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-14bvioie93k8i/spring-petclinic-2.7.3.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
