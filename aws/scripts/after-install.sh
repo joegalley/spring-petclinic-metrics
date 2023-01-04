35b95d8f899c4c6fd91c98790b9984cf350477d9
Joe Galley <joegalley@users.noreply.github.com>
2023-01-04 11:03:54 -0500

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-3dh4ysaq37q7/app.war /usr/local/tomcat9/webapps/app.war
+aws s3 cp s3://observabilitydemostack-webappdeploymentbucket-18ocyoo6w7enz/app.war /usr/local/tomcat9/webapps/app.war
 
 
 # Ensure the ownership permissions are correct.
